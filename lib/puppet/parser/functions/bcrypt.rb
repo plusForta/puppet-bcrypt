5451563d9971529f56bec34dd8c56b897a7fa1e7
Tim Rogers <tiroger@microsoft.com>
2014-05-08 15:53:17 -0700

    correcting for function argument parsing error

--- lib/puppet/parser/functions/bcrypt.rb
+++ lib/puppet/parser/functions/bcrypt.rb
@@ -1,6 +1,11 @@
 module Puppet::Parser::Functions
   newfunction(:bcrypt, :type => :rvalue) do |plaintext|
+
+    if (plaintext.size != 1) then
+      raise(Puppet::ParseError, "bcrypt(): Wrong number of arguments: given #{plaintext.size}, expected 1")
+    end
+
     require 'bcrypt'
-    BCrypt::Password.create(plaintext).to_s
+    BCrypt::Password.create(plaintext[0]).to_s
   end
 end
