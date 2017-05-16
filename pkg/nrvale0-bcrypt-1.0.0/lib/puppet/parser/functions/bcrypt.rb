00227c02edfdf6463eebc626b06bfac8031710fd
Igor Galić <i.galic@brainsware.org>
2017-05-16 10:51:12 +0200

    add .gitignore, ignore (and remove) pkg/

--- pkg/nrvale0-bcrypt-1.0.0/lib/puppet/parser/functions/bcrypt.rb
+++
@@ -1,6 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:bcrypt, :type => :rvalue) do |plaintext|
-    require 'bcrypt'
-    BCrypt::Password.create(plaintext).to_s
-  end
-end
--- pkg/nrvale0-bcrypt-1.0.0/tests/bcrypt.pp
+++
@@ -1,6 +0,0 @@
-$plaintext = 'foobar'
-$bcryptpasswd = bcrypt($plaintext)
-
-notify { 'test brcypt()':
-  message => "plain-text: $plaintext ; bcrypt: $bcryptpasswd",
-}
